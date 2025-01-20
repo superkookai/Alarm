eca84921a24963018d8b9daa90f0276eeba96d29
Weerawut <weerawut@live.com>
2025-01-20 10:42:48 +0700

    Initial Commit

---
+++ Alarm/AlarmApp.swift
@@ -0,0 +1,17 @@
+//
+//  AlarmApp.swift
+//  Alarm
+//
+//  Created by Weerawut Chaiyasomboon on 20/1/2568 BE.
+//
+
+import SwiftUI
+
+@main
+struct AlarmApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
